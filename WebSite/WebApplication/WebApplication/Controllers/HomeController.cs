ac272f2b9c1d4f4d9003d80cff00ad7917c6e26a
moiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2015-10-31 01:53:37 +0300

    fixedbd

--- WebSite/WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebSite/WebApplication/WebApplication/Controllers/HomeController.cs
@@ -16,13 +16,11 @@ namespace WebApplication.Controllers
         public ActionResult Index()
         {
             List<Demotivator> model = new List<Demotivator>();
-          //  List<Comment> model1 = new List<Comment>();
-            using (var context = new Entities())
+                   using (var context = new Entities())
                    {
                     model = context.Demotivators.ToList();
-              //  model1 = context.Comments.ToList();
                    }
-                 return View(model);
+            return View(model);
         }
 
         public ActionResult About()
---
+++ WebSite/WebApplication/WebApplication/Scripts/UploadFile.js
@@ -0,0 +1,37 @@
+﻿var canvas = new fabric.Canvas("canvas");
+$(document).ready(function () {
+    $("#uploadFileBtn")[0].onchange = UploadImageFromComputer;
+    $("#PreviewBtn")[0].onclick = UploadImageFromUrl;
+});
+
+function UploadImageFromComputer()
+{
+    var temp = $('#uploadFileBtn')[0].files[0];
+    var Url = URL.createObjectURL(temp);
+    fabric.Image.fromURL(Url, function (oImg) {
+        canvas.clear();
+        canvas.setBackgroundImage(oImg);
+        InitializeCanvas();
+        canvas.backgroundImage.setWidth(canvas.getWidth() - 10);
+        canvas.backgroundImage.setHeight(canvas.getHeight() - 10);
+        canvas.renderAll.bind(canvas);
+    });
+}
+function UploadImageFromUrl()
+{
+    fabric.Image.fromURL($('#ImageUrl')[0].value, function (oImg) {
+        canvas.clear();
+        InitializeCanvas();
+        canvas.setBackgroundImage(oImg, canvas.renderAll.bind(canvas));
+        canvas.backgroundImage.setWidth(canvas.getWidth() - 10);
+        canvas.backgroundImage.setHeight(canvas.getHeight() - 10);
+        canvas.renderAll.bind(canvas);
+    });
+}
+function InitializeCanvas()
+{
+    canvas.setWidth(500);
+    canvas.setHeight(300);
+    //canvas.setbackgroundColor('#00000');
+    canvas.renderAll.bind(canvas);
+}
